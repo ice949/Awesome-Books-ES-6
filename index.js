30cf1f02dda4e23559c716e50f16683453d309e2
ice949 <justineimasiku21@gmail.com>
2022-07-05 15:06:49 +0200

    Create awesome books with ES-6 and Modules

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import { switchPage } from './modules/switcher.js';
+import switchPage from './modules/switcher.js';
 import addAndRemoveBook from './modules/app.js';
 
-
-
-const date = document.querySelector('.time');
-
+const date = document.getElementById('time');
+window.setInterval(() => {
+  // eslint-disable-next-line no-undef
+  date.innerHTML = `<p>${luxon.DateTime.local().toLocaleString(luxon.DateTime.DATETIME_MED_WITH_SECONDS)
+  }</p>`;
+}, 1000, this);
 
 switchPage();
 addAndRemoveBook();
-// date.textContent = currentDate();
